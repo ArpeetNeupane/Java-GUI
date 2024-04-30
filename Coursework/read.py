2794affe93510ae31938667eda3fe689d2fce741
InfernamGlutton <neupanearpeet@gmail.com>
2024-04-30 18:08:55 +0545

    ver 1.1

--- Coursework/read.py
+++ Coursework/read.py
@@ -9,19 +9,20 @@ def readingFile():
     Returns:
     Dictionary: d, keys are integers and values are lists.
     """
+    
     file = open("land.txt", "r")
     lines = file.readlines()
     d = {}
     for line in lines:
-        line = line.replace("\n", "")
-        list_ = line.split(",")
+        line = line.replace("\n", "") #removing new line at the end of each line using replace  function
+        list_ = line.split(",") #splitting into parts wherever , is found
         
-        key = int(list_[0])
+        key = int(list_[0]) #the first item of each line is assigned to variabe 'key'
         value=[]
         for i in range(1,len(list_)):
-            value.append(list_[i])
+            value.append(list_[i]) #list named 'value' has all datas except first one
         
-        d[key] = value
+        d[key] = value #in dictionary d, each key is assigned it's value like it was in the text file
     file.close
     return d
 
